850308f46d963e788beeea7c39f0c511c8bb6f4c
Akash  Gauda <109796988+gaudakash@users.noreply.github.com>
2026-03-31 15:52:20 +0000

    learning console.log

---
+++ 01_Basics/variable.js
@@ -0,0 +1,19 @@
+const userId= "125";
+let userOne = "dinesh";
+var userEmail = "test@gmail.com";
+choudhary = "kutch nahi";
+
+
+// now chekhed thae vaiable which will changes made doen 
+
+// due to scoping issues 
+// scope are like this 
+
+// {}
+
+// var not used 
+
+const userId1 = "122"  
+let userOne1 = "nareh";
+var userEmail1 = "dinsesh@@mail.com"
+console.table([userId1, userOne1, userEmail1])
